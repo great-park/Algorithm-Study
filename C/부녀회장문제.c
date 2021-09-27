551b07fc0a2489773e812472096a986399f699f1
great-park <cksgh1735@naver.com>
2021-09-28 01:07:23 +0900

    maximum subarray using Divide and Conquer

--- C/부녀회장문제.c
+++ C/부녀회장문제.c
@@ -1,30 +1,16 @@
 #include <stdio.h>
-int result(int k, int n, int r);
-int main() {
-    int t, k, n, r;
-    t = 1; r = 0;
-    scanf("%d", &t);
-    while (t != 0) {
-        scanf("%d\n%d", &k, &n);
-        printf("%d\n", result(k, n, r));
 
-        t--;
+int main() {
+
+    int x;
+    scanf("%d\n", &x);
+    int str[x];
+    for (int i = 0; i < x; i++) {
+        scanf("%d\n", &str[i]);
     }
+
+    printf("%c", str);
 
 
     return 0;
-}
-int result(int k, int n, int r) {
-    if (k == 1) {
-        for (int i = 1; i < n + 1; i++) {
-            r += i;
-        }
-        return r;
-    }
-    else {
-        for (int i = 1; i < n + 1; i++) {
-            r += result(k - 1, i, 0);
-        }
-        return r;
-    }
 }
