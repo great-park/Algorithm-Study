c9a34cbe58bac3318f3a1ca77af1f566e51e7b34
great-park <cksgh1735@naver.com>
2022-01-27 23:56:11 +0900

    정리

--- 연습장.py
+++ 연습장.py
@@ -1,20 +1,12 @@
-from collections import deque
-N, K = map(int, input().split())
-queue = deque()
-answer = []
-for i in range(1, N+1):
-    queue.append(i)
+n, m = list(map(int, input().split(' ')))
+data = list(map(int, input().split(' ')))
 
-while queue:
-    for i in range(K-1):
-        queue.append(queue.popleft())
+max_value = 0
 
-    answer.append(queue.popleft())
-
-print("<", end='')
-
-for i in range(N-1):
-    print(str(answer[i])+", ", end='')
-print(answer[N-1], end='')
-
-print(">")
+for i in range(n):
+    for j in range(i+1, n):
+        for k in range(j+1, n):
+            sum = data[i]+data[j]+data[k]
+            if sum <= m:
+                max_value = max(max_value, sum)
+print(max_value)
