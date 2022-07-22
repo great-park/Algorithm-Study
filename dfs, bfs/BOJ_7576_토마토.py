4fbd4d7e18a07af71e2b8ca87a63de9f42c07fda
great-park <cksgh1735@naver.com>
2022-07-22 16:48:34 +0900

    .

--- dfs, bfs/BOJ_7576_토마토.py
+++ dfs, bfs/BOJ_7576_토마토.py
@@ -1,4 +1,5 @@
 import sys
+from collections import deque
 input = sys.stdin.readline
 M, N = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(N)]
@@ -10,3 +11,35 @@ queue = deque()
 
 def BFS():
     while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx, ny = x+dx[i], y+dy[i]
+            if 0 <= nx < N and 0 <= ny < M:
+                if graph[nx][ny] == 0:
+                    graph[nx][ny] = graph[x][y] + 1
+                    queue.append((nx, ny))
+
+
+for i in range(N):
+    for j in range(M):
+        if graph[i][j] == 1:
+            queue.append((i, j))
+cannot_complete = False
+day = 0
+
+
+def solve():
+    global cannot_complete, day
+    BFS()
+    for i in range(N):
+        for j in range(M):
+            if graph[i][j] == 0:
+                cannot_complete = True
+            day = max(day, graph[i][j])
+
+
+solve()
+if cannot_complete:
+    print(-1)
+else:
+    print(day-1)
