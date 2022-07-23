f5518e85f42f5926432e5a5e6a40588cd2c23c26
great-park <cksgh1735@naver.com>
2022-07-24 00:38:16 +0900

    성공

--- Queue/BOJ_5430_AC.py
+++ Queue/BOJ_5430_AC.py
@@ -2,6 +2,7 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 error_flag = 0
+R_num = 0
 
 
 def R(arr):
@@ -11,26 +12,36 @@ def R(arr):
 
 def D(arr):
     global error_flag
-    if len(arr) == 0:
+    if len(arr) == 0 or arr == deque(['']):
         error_flag = 1
-    else:
+    # 짝수번 뒤집었기 때문에 뒤에서 제거
+    elif R_num % 2 == 0:
         arr.popleft()
+    # 홀수번 뒤집었기 때문에 앞에서 제거
+    else:
+        arr.pop()
     return
 
 
 def solve():
-    func = list(input().rstrip())
+    global error_flag
+    global R_num
+    error_flag = 0
+    R_num = 0
+    p = list(input().rstrip())
     n = int(input())
     arr = deque(sys.stdin.readline().rstrip()[1:-1].split(","))
-    for operator in func:
+    for operator in p:
         if operator == 'R':
-            R(arr)
+            R_num += 1
         elif operator == 'D':
             D(arr)
-
     if error_flag == 1:
         print("error")
+    elif R_num % 2 == 0:
+        print("["+",".join(arr)+"]")
     else:
+        R(arr)
         print("["+",".join(arr)+"]")
 
 
