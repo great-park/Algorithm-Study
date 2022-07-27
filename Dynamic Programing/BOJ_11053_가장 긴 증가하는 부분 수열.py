32caac15fbb2fa70db933eea98ac4067a2dc86d1
great-park <cksgh1735@naver.com>
2022-07-27 15:29:23 +0900

    .

---
+++ Dynamic Programing/BOJ_11053_가장 긴 증가하는 부분 수열.py
@@ -0,0 +1,12 @@
+from sys import stdin
+input = stdin.readline
+N = int(input())
+num = list(map(int, input().split()))
+dp = [1]*N
+
+for i in range(N):
+    for j in range(i):
+        if num[i] > num[j]:
+            dp[i] = max(dp[i], dp[j]+1)
+
+print(max(dp))
