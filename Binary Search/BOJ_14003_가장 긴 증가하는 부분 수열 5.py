9ba35d2c93db6af7a6199ae516db05c35f78b721
great-park <cksgh1735@naver.com>
2022-07-27 15:04:07 +0900

    실패

--- Binary Search/BOJ_14003_가장 긴 증가하는 부분 수열 5.py
+++ Binary Search/BOJ_14003_가장 긴 증가하는 부분 수열 5.py
@@ -17,3 +17,13 @@ for i in range(1, n + 1):
         tmp[dp[i]] = a[i]  # 해당 위치의 수를 현재 수로 갱신해준다.
 
 print(maxVal)
+
+# 연산이 완료되면 dp[i]에는 a[i]가 마지막인 부분 수열의 최대 길이가 저장되어있다.
+tmp2 = []
+for i in range(n, 0, -1):
+    if dp[i] == maxVal:
+        tmp2.append(a[i])
+        maxVal -= 1
+print(*tmp2[::-1])
+
+# 출처 : https://ca.ramel.be/105
