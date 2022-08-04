db9b676f7b48f96a6448d46f9a9d1c36b7206d47
great-park <cksgh1735@naver.com>
2022-08-04 14:26:53 +0900

    DP

---
+++ Dynamic Programing/BOJ_9095_1,2,3더하기.py
@@ -0,0 +1,29 @@
+from sys import stdin
+input = stdin.readline
+N = int(input())
+"""
+패턴 찾기
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4  : 1+1+1, 1+2, 2+1, 3
+dp[4] = 7  : 1+1+1+1,  1+1+2, 1+2+1, 2+1+1, 2+2, 1+3, 3+1
+dp[5] = 13 : 1+1+1+1+1, 1+2 * 5개, 2+2+1 * 3개, 3+1+1 *3게, 3+2
+dp[6] = 24 : 1+1+1+1+1+1, 1+2*5개, 2+2+1+1 * 6개, 3+1+1+1*4개, 3+2+1 6개, 3+3 1개
+dp[7] = 44
+"""
+
+
+def solve(x):
+    if x == 1:
+        return 1
+    elif x == 2:
+        return 2
+    elif x == 3:
+        return 4
+    else:
+        return solve(x-1) + solve(x-2) + solve(x-3)
+
+
+for i in range(N):
+    num = int(input())
+    print(solve(num))
