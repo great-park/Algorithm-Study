c4d9313810f72f221298f28a9e1e8db7ac16b770
great-park <cksgh1735@naver.com>
2022-08-05 14:21:27 +0900

    백트래킹

---
+++ BackTracking/BOJ_15655_N과 M(6).py
@@ -0,0 +1,24 @@
+from sys import stdin
+input = stdin.readline
+N, M = map(int, input().split())
+num = sorted(list(map(int, input().split())))
+ans = []
+
+
+def solve():
+    if len(ans) == M:
+        print(' '.join(map(str, ans)))
+        return
+    for i in range(N):
+        if len(ans) == 0:
+            ans.append(num[i])
+            solve()
+            ans.pop()
+        else:
+            if num[i] >= ans[-1]:
+                ans.append(num[i])
+                solve()
+                ans.pop()
+
+
+solve()
--- BackTracking/N_Queen.py
+++ BackTracking/N_Queen.py
@@ -35,3 +35,39 @@ def solve_n_queens(N):
 
 
 solve_n_queens(n)
+#
+"""
+promising 조건
+1. 같은 열 체크 :  col[i] == col[k]
+2. 대각선 체크  :  abs(col[i]-col[k]) == i-k
+"""
+count = 0
+
+
+def promising(i, col):
+    k = 1
+    flag = True
+    while (k < i and flag):
+        if (col[i] == col[k] or abs(col[i]-col[k]) == (i-k)):
+            flag = False
+        k += 1
+    return flag
+
+
+def n_queens(i, col):
+    global count
+    n = len(col) - 1
+    if (promising(i, col)):
+        if(i == n):
+            count += 1
+            # print(col[1:n+1])
+        else:
+            for j in range(1, n+1):
+                col[i+1] = j
+                n_queens(i+1, col)
+
+
+x = int(input())
+col = [0] * (x+1)
+n_queens(0, col)
+print(count)
