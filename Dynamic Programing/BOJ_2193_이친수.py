b046d63537d09db19338ee6120d9522811647524
great-park <cksgh1735@naver.com>
2022-09-05 18:44:48 +0900

    dp

--- Dynamic Programing/BOJ_2193_이친수.py
+++ Dynamic Programing/BOJ_2193_이친수.py
@@ -1,15 +1,4 @@
-from sys import stdin
-input = stdin.readline
 N = int(input())
-
-"""
-n
-1 - 1
-2 - 10
-3 - 101, 100
-4 - 1010, 1001, 1000
-5 - 10101, 10100, 10010, 10001, 10000
-"""
 dp = [0 for _ in range(N+1)]
 dp[1] = 1
 dp[2] = 1
