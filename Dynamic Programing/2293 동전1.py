76974ba863d8d10e835429db58517065b20b5337
great-park <cksgh1735@gmail.com>
2023-02-06 20:52:47 +0900

    fix: 2293 주석 추가

--- Dynamic Programing/2293 동전1.py
+++ Dynamic Programing/2293 동전1.py
@@ -10,7 +10,8 @@ dp = [0] * (k+1)
 dp[0] = 1
 
 for value in values:
-    for i in range(1, k+1):
+    for i in range(1, k+1):  # i원
         if i - value >= 0:
-            dp[i] += dp[i-value]
+            dp[i] += dp[i-value]  # i-value원을 만드는 경우의 수만큼 추가
+    # print(dp)
 print(dp[k])
