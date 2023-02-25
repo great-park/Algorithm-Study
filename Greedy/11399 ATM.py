a9181230923fea42ddd8c507edc975b297ad8621
great-park <cksgh1735@gmail.com>
2023-02-25 22:20:02 +0900

    feat: Greedy - BOJ_1789, 11399

--- Greedy/11399 ATM.py
+++ Greedy/11399 ATM.py
@@ -1,11 +1,23 @@
+# from sys import stdin
+# input = stdin.readline
+# N = int(input())
+# time = list(map(int, input().split()))
+# time.sort()
+# sum = 0
+# result = 0
+# for i in range(N):
+#     sum += time[i]
+#     result += sum
+# print(result)
 from sys import stdin
 input = stdin.readline
 N = int(input())
 time = list(map(int, input().split()))
 time.sort()
-sum = 0
-result = 0
-for i in range(N):
-    sum += time[i]
-    result += sum
-print(result)
+res = 0
+for id, value in enumerate(time):
+    if id == 0:
+        res += value
+    else:
+        time[id] = value + time[id-1]
+print(sum(time))
